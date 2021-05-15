ced5f69877842df5e931c93fafc9f4f1b59c6974
Roshaan Siddiqui <roshaansiddiqui@Roshaans-MacBook-Pro-3.local>
2021-05-15 17:05:19 -0400

    database config

--- django_music_service/settings.py
+++ django_music_service/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'pb7hfzbyz$&rn#ouho)gxgnd4yl=t+7vvagq!gj1b+!y!8!2e$'
-SECRET_KEY = config('configuration')
+# SECRET_KEY = ''
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -96,8 +96,8 @@ WSGI_APPLICATION = 'django_music_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
